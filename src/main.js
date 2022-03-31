9c996f58c5c464b37cf9d072c28b7ddaaa2015bc
胡赞來 <361206768@qq.com>
2022-03-31 09:21:13 +0800

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+
 
 Vue.config.productionTip = false
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increse (state) {
+      state.count++
+    },
+    reduce (state) {
+      state.count--
+    }
+  }
+})
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
